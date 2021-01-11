6beeba7e41e79f8badf7660756019bce31b73fad
Chinedu <okoriechinedusunday@gmail.com>
2021-01-11 16:11:32 +0100

    clean up action file

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,11 @@ import ResturantReview from '../src/components/resturant';
 import logo from '../src/img/BON-hotel-Logo.png';
 import { Typography, Button, Image } from 'antd';
 import { RightOutlined, LeftOutlined } from '@ant-design/icons';
+
+import store from './store';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import './App.css';
 const { Title, Text } = Typography;
 
@@ -15,78 +20,86 @@ function App() {
 	const [response, setResponse] = useState({});
 
 	return (
-		<main className='app-container'>
-			<div className='content-container'>
-				<div>
-					<Image src={logo} />
-				</div>
-				<Title className='title'>Guest Review</Title>
-				<Text>Please submit a review so we can serve you better</Text>
-				{progressCount === 1 && (
-					<ReceptionReview
-						progressCount={progressCount}
-						response={response}
-						setResponse={setResponse}
-						facilitiesUsed={facilitiesUsed}
-						setFacilitiesUsed={setFacilitiesUsed}
-						reviews={reviews}
-						setReviews={setReviews}
-					/>
-				)}
-				{progressCount === 2 && (
-					<PoolReview
-						progressCount={progressCount}
-						response={response}
-						setResponse={setResponse}
-						facilitiesUsed={facilitiesUsed}
-						setFacilitiesUsed={setFacilitiesUsed}
-						reviews={reviews}
-						setReviews={setReviews}
-					/>
-				)}
-				{progressCount === 3 && (
-					<ResturantReview
-						progressCount={progressCount}
-						response={response}
-						setResponse={setResponse}
-						facilitiesUsed={facilitiesUsed}
-						setFacilitiesUsed={setFacilitiesUsed}
-						reviews={reviews}
-						setReviews={setReviews}
-					/>
-				)}
+		<Provider store={store}>
+			<main className='app-container'>
+				<div className='content-container'>
+					<div>
+						<Image src={logo} />
+					</div>
+					<Title className='title'>Guest Review</Title>
+					<Text>
+						Please submit a review so we can serve you better
+					</Text>
+					{progressCount === 1 && (
+						<ReceptionReview
+							progressCount={progressCount}
+							response={response}
+							setResponse={setResponse}
+							facilitiesUsed={facilitiesUsed}
+							setFacilitiesUsed={setFacilitiesUsed}
+							reviews={reviews}
+							setReviews={setReviews}
+						/>
+					)}
+					{progressCount === 2 && (
+						<PoolReview
+							progressCount={progressCount}
+							response={response}
+							setResponse={setResponse}
+							facilitiesUsed={facilitiesUsed}
+							setFacilitiesUsed={setFacilitiesUsed}
+							reviews={reviews}
+							setReviews={setReviews}
+						/>
+					)}
+					{progressCount === 3 && (
+						<ResturantReview
+							progressCount={progressCount}
+							response={response}
+							setResponse={setResponse}
+							facilitiesUsed={facilitiesUsed}
+							setFacilitiesUsed={setFacilitiesUsed}
+							reviews={reviews}
+							setReviews={setReviews}
+						/>
+					)}
 
-				<div className='previous-next-container'>
-					{progressCount === 1 ? null : (
-						<Button
-							onClick={() => setProgressCount(progressCount - 1)}
-							className='action-btn'
-							type='primary'
-							icon={<LeftOutlined />}
-							shape='round'></Button>
-					)}
-					{progressCount >= 3 ? (
-						<Button
-							className='action-btn'
-							type='primary'
-							shape='round'>
-							Submit
-							<RightOutlined />
-						</Button>
-					) : (
-						<Button
-							onClick={() => setProgressCount(progressCount + 1)}
-							className='action-btn'
-							type='primary'
-							icon={<RightOutlined />}
-							shape='round'></Button>
-					)}
+					<div className='previous-next-container'>
+						{progressCount === 1 ? null : (
+							<Button
+								onClick={() =>
+									setProgressCount(progressCount - 1)
+								}
+								className='action-btn'
+								type='primary'
+								icon={<LeftOutlined />}
+								shape='round'></Button>
+						)}
+						{progressCount >= 3 ? (
+							<Button
+								className='action-btn'
+								type='primary'
+								shape='round'>
+								Submit
+								<RightOutlined />
+							</Button>
+						) : (
+							<Button
+								onClick={() =>
+									setProgressCount(progressCount + 1)
+								}
+								className='action-btn'
+								type='primary'
+								icon={<RightOutlined />}
+								shape='round'></Button>
+						)}
+					</div>
+					<div className='progress-count'>
+						{progressCount > 3 ? 3 : progressCount} 0f 3
+					</div>
 				</div>
-				<div className='progress-count'>
-					{progressCount > 3 ? 3 : progressCount} 0f 3
-				</div>
-			</div>
-		</main>
+			</main>
+		</Provider>
 	);
 }
 
