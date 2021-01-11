1da20d2497e01e895112c31e0e368e61a0f3f113
Chinedu <okoriechinedusunday@gmail.com>
2021-01-11 12:22:35 +0100

    add progress count

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,51 @@
 import { useState } from 'react';
 import ReceptionReview from '../src/components/receptionist';
 import { Typography, Button } from 'antd';
-import { RightOutlined } from '@ant-design/icons';
+import { RightOutlined, LeftOutlined } from '@ant-design/icons';
 import './App.css';
 const { Title, Text } = Typography;
 
 function App() {
 	const [view, setView] = useState(1);
 	const [progressCount, setProgressCount] = useState(1);
+
 	const [response, setResponse] = useState({});
+	console.log(progressCount);
 	return (
 		<main className='app-container'>
 			<div className='content-container'>
 				<Title className='title'>Quest Review</Title>
 				<Text>Please submit a review so we can serve you better</Text>
 				<ReceptionReview />
-				<div>
-					<Button
-						className='action-btn'
-						type='primary'
-						icon={<RightOutlined />}
-						shape='round'></Button>
+				<div className='previous-next-container'>
+					{progressCount === 1 || progressCount === 11 ? null : (
+						<Button
+							onClick={() => setProgressCount(progressCount - 1)}
+							className='action-btn'
+							type='primary'
+							icon={<LeftOutlined />}
+							shape='round'></Button>
+					)}
+					{progressCount === 11 ? (
+						<Button
+							className='action-btn'
+							type='primary'
+							shape='round'>
+							Submit
+							<RightOutlined />
+						</Button>
+					) : (
+						<Button
+							onClick={() => setProgressCount(progressCount + 1)}
+							className='action-btn'
+							type='primary'
+							icon={<RightOutlined />}
+							shape='round'></Button>
+					)}
 				</div>
-				<div className='progress-count'>1 0f 10</div>
+				<div className='progress-count'>
+					{progressCount > 10 ? 10 : progressCount} 0f 10
+				</div>
 			</div>
 		</main>
 	);
